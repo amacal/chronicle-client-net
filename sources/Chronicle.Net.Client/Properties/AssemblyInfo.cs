6843160cf754b961c24c80af2265f88cd226db03
Adrian Macal <adma@amacal.pl>
2017-05-15 19:24:59 +0200

    Initial commit.

---
+++ sources/Chronicle.Net.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Chronicle.Net.Client")]
+[assembly: AssemblyProduct("Chronicle.Net.Client")]
+[assembly: AssemblyCopyright("Copyright © Adrian Macal 2017")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
